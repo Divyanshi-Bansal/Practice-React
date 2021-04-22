9bbf2cac9abfab1cabd85b2c009134bf6d6115f1
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2021-04-22 23:51:00 +0530

    render elements

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react'; //we using this module just because of we use jsx expression.
+import ReactDOM from 'react-dom';
 // import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
@@ -16,7 +16,9 @@
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
 
-var React = require('react');
-var ReactDom = require('react-dom');
 
-ReactDOM.render(<h1>Hello world</h1> , document.getElementById('root'));
+ReactDOM.render(
+  [ <h1>Hello world</h1>,
+  <p>lorem10</p>
+
+ ] , document.getElementById('root'));
