7dd5e75d6b54dd766222eb8e419f448103bcfc7f
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2021-04-25 00:51:45 +0530

    change heading

--- src/Practice/Heading.jsx
+++ src/Practice/Heading.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function Heading(){
-    return <h1>This is a heading.</h1>
+    return <h1>This is a Heading.</h1>
 }
 
 export default Heading;
