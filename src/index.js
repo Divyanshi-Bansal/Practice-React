a97194b72a79a8c55b0c5523a04dbdbf8213fa2a
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2021-04-27 02:16:48 +0530

    change link and name of series

--- src/index.js
+++ src/index.js
@@ -19,8 +19,8 @@ ReactDOM.render(
   // </React.StrictMode>
   <div>
   <NetflixCards imgsrc='https://upload.wikimedia.org/wikipedia/en/2/22/TVD_cast.jpg' category='A NETFLIX ORIGINAL SERIES' title='The Vampire Dairies' link='https://www.netflix.com/in/' />  
-  <NetflixCards imgsrc='https://upload.wikimedia.org/wikipedia/en/2/22/TVD_cast.jpg' category='A NETFLIX ORIGINAL SERIES' title='The Vampire Dairies' link='https://www.netflix.com/in/' />  
-  <NetflixCards imgsrc='https://upload.wikimedia.org/wikipedia/en/2/22/TVD_cast.jpg' category='A NETFLIX ORIGINAL SERIES' title='The Vampire Dairies' link='https://www.netflix.com/in/' />  
+  <NetflixCards imgsrc='https://pdqwire.com/wp-content/uploads/2020/08/35285-1024x575.jpg' category='A NETFLIX ORIGINAL SERIES' title='SEX Education' link='https://www.netflix.com/in/' />  
+  <NetflixCards imgsrc='https://images-na.ssl-images-amazon.com/images/I/51MmI%2BNl5DL._AC_.jpg' category='A NETFLIX ORIGINAL SERIES' title='Dark' link='https://www.netflix.com/in/' />  
   </div>
   ,
   document.getElementById('root')
